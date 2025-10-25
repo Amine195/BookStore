142c19424bd8a13a3402c5b1bab722ef95cdb98b
Mohammed Amine GZL <gmadhgs@gmail.com>
2025-10-25 09:31:54 -0400

    Update Book

--- src/main/java/bookstore/project/controller/BookController.java
+++ src/main/java/bookstore/project/controller/BookController.java
@@ -3,12 +3,8 @@ package bookstore.project.controller;
 import bookstore.project.beans.Book;
 import bookstore.project.dao.BookDAO;
 import bookstore.project.dao.IBookDAO;
-import static com.sun.org.apache.xerces.internal.util.XMLChar.trim;
-
 import java.io.IOException;
 import java.sql.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -94,10 +90,61 @@ public class BookController extends HttpServlet {
         // 4. Enregistrer le livre via le DAO
         boolean success = bookDAO.addBook(newBook);
         System.out.println(success);
+        
+        // 5. Retourner la page JSP
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Pages/books/bookList.jsp");
+        try
+        {
+            dispatcher.forward(request, response);
+        }
+        catch(ServletException | IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private void updateBook(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("updateBook");
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        // 1. Récupération et nettoyage des paramètres
+        String title = request.getParameter("title");
+        String author = request.getParameter("author");
+        String publisher = request.getParameter("publisher");
+        String publicationDateStr = request.getParameter("publicationDate");
+        String category = request.getParameter("category");
+        String language = request.getParameter("language");
+        String pagesStr = request.getParameter("pages");
+        String format = request.getParameter("format");
+        String priceStr = request.getParameter("price");
+        String stockStr = request.getParameter("stock");
+        String description = request.getParameter("description");
+        
+        // 2. Convertir les types       
+        int pages = Integer.parseInt(pagesStr);
+        double price = Double.parseDouble(priceStr);
+        int stock = Integer.parseInt(stockStr);
+        Date publicationDate = Date.valueOf(publicationDateStr);
+        
+        // 3. Créer le Book
+        Book newBook = new Book(
+            id, title, author, publisher, publicationDate, category,
+            language, pages, format, price, stock, description
+        );
+        
+        // 4. Enregistrer le livre via le DAO
+        boolean success = bookDAO.updateBook(newBook);
+        System.out.println(success);
+        
+        // 5. Retourner la page JSP
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Pages/books/bookList.jsp");
+        try
+        {
+            dispatcher.forward(request, response);
+        }
+        catch(ServletException | IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private void deleteBook(HttpServletRequest request, HttpServletResponse response) {
---
+++ src/main/webapp/Pages/books/bookList.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ include file="/Partials/header.jsp" %>
+
+<main class="container mb-3">
+    <section>
+        <h1>Book list page</h1>
+    </section>
+</main>
+
+<%@ include file="/Partials/footer.jsp" %>
